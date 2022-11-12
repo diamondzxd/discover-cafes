123ae26d9e66951a7e25db42c4a4166e67016e65
Piyush Dhall <contact@piyush.ovh>
2022-11-13 02:32:34 +0530

    app setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Footer from "./footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
